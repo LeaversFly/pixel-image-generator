25583b884e70d1e45cb2f489169130cc7d36490a
Gin_99 <3348483879@qq.com>
2023-06-16 23:15:11 +0800

    ci: 构建页面

--- src/views/home/components/generator/index.vue
+++ src/views/home/components/generator/index.vue
@@ -1,123 +1,118 @@
 <script setup>
 import { CloudSyncOutlined, CloudUploadOutlined } from '@vicons/antd'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { createPixelator } from '../../../../utils/pixelator'
+import useLastedToken from '../../../../hooks/useLasteToken'
+import { useMessage } from 'naive-ui'
 
-// const AppState = {
-//     Normal: 'Normal',
-//     Loading: 'Loading',
-//     Generating: 'Generating',
-// }
+const message = useMessage()
 
-// const imageData = ref(null)
-// const appState = ref(AppState.Normal)
-// const hasResult = ref(false)
-// const canvasRef = ref(null)
+const AppState = {
+    Normal: 'Normal',
+    Loading: 'Loading',
+    Generating: 'Generating',
+}
 
-// //useCallback的用法
-// const onPixelatorUpdate = useCallback((finish) => {
-//     canvasRef.current && pixelatorRef.value.current.toCanvas(canvasRef.current)
-//     hasResult.value = true
-//     finish && (appState.value = AppState.Normal)
-// }, [])
+const imageData = ref(null)
+const appState = ref(AppState.Normal)
+const hasResult = ref(false)
+const canvasRef = ref(null)
+const pixelatorRef = ref(createPixelator(onPixelatorUpdate))
 
-// const pixelatorRef = ref(createPixelator(onPixelatorUpdate))
-// pixelatorRef.value.current.onUpdate = onPixelatorUpdate
+const onPixelatorUpdate = (finish) => {
+    canvasRef.value && pixelatorRef.value.toCanvas(canvasRef.value)
+    hasResult.value = true
+    finish && (appState.value = AppState.Normal)
+}
 
-// //useEffect的用法
-// useEffect(() => {
-//     if (canvasRef.current) {
-//         if (imageData.value) {
-//             const canvas = canvasRef.value.current
-//             const ctx = canvas.getContext('2d')
-//             canvas.width = imageData.value.width
-//             canvas.height = imageData.value.height
-//             ctx.putImageData(imageData.value, 0, 0)
-//         }
-//     }
-// }, [imageData])
+pixelatorRef.value.onUpdate = onPixelatorUpdate
 
-// const { getLastedToken, comsumeToken } = useLastedToken()
+onMounted(() => {
+    if (canvasRef) {
+        if (imageData.value) {
+            const canvas = canvasRef.value
+            const ctx = canvas.getContext('2d')
+            canvas.width = imageData.value.width
+            canvas.height = imageData.value.height
+            ctx.putImageData(imageData.value, 0, 0)
+        }
+    }
+})
 
-// //useCallback的用法
-// const tryLoadBlob = useCallback(
-//     async (blob, token) => {
-//         const url = URL.createObjectURL(blob)
-//         try {
-//             const imageData = await GetImageDataFromSrc(url)
-//             comsumeToken(token, () => {
-//                 imageData.value = imageData
-//                 hasResult.value = false
-//                 appState.value = AppState.Normal
-//             })
-//         } catch {
-//             comsumeToken(token, () => {
-//                 setAppState(AppState.Normal)
-//             })
-//             Toast.error('图片解析错误')
-//         }
-//         URL.revokeObjectURL(url)
-//     },
-//     [comsumeToken],
-// )
+const { getLastedToken, comsumeToken } = useLastedToken()
 
-// const importDropdownVisible = ref(false)
+const tryLoadBlob = async (blob, token) => {
+    const url = URL.createObjectURL(blob)
+    try {
+        const imageData = await GetImageDataFromSrc(url)
+        comsumeToken(token, () => {
+            imageData.value = imageData
+            hasResult.value = false
+            appState.value = AppState.Normal
+        })
+    } catch {
+        comsumeToken(token, () => {
+            appstate.value = AppState.Normal
+        })
+        message.error('图片解析错误!')
+    }
+    URL.revokeObjectURL(url)
+}
 
-// //useCallback的用法
-// const onClickImport = useCallback(() => {
-//     importDropdownVisible.value = true
-// }, [])
-// const onClickImportOutside = useCallback(() => {
-//     importDropdownVisible.value = false
-// }, [])
-// const onImportFromFile = useCallback(() => {
-//     importDropdownVisible.value = false
-//     const token = getLastedToken()
-//     const input = document.createElement('input')
-//     input.type = 'file'
-//     input.accept = 'image/*'
-//     input.click()
-//     input.onchange = async () => {
-//         if (input.files && input.files.length > 0) {
-//             appState.value = AppState.Loading
-//             await tryLoadBlob(input.files.item(0), token)
-//         }
-//     }
-// }, [getLastedToken, tryLoadBlob])
+const importDropdownVisible = ref(false)
 
-// const onImportFromClipboard = useCallback(async () => {
-//     importDropdownVisible.value = false
-//     appState.value = AppState.Loading
+const onClickImport = () => {
+    importDropdownVisible.value = true
+}
+const onClickImportOutside = () => {
+    importDropdownVisible.value = false
+}
+const onImportFromFile = () => {
+    importDropdownVisible.value = false
+    const token = getLastedToken()
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = 'image/*'
+    input.click()
+    input.onchange = async () => {
+        if (input.files && input.files.length > 0) {
+            appState.value = AppState.Loading
+            await tryLoadBlob(input.files.item(0), token)
+        }
+    }
+}
 
-//     const token = getLastedToken()
-//     try {
-//         const items = await navigator.clipboard.read()
-//         if (items.length > 0) {
-//             const item = items.at(0)
-//             const blob = await item.getType(item.types[0])
-//             await tryLoadBlob(blob, token)
-//         } else {
-//             Toast.error('未读取到内容')
-//         }
-//     } catch {
-//         Toast.error('读取剪切板失败')
-//     }
-// }, [getLastedToken, tryLoadBlob])
+const onImportFromClipboard = async () => {
+    importDropdownVisible.value = false
+    appState.value = AppState.Loading
 
-// const onImportOk = useCallback(
-//     async ({ url }) => {
-//         setAppState(AppState.Loading)
-//         const token = getLastedToken()
-//         try {
-//             const response = await fetch(url)
-//             const blob = await response.blob()
-//             await tryLoadBlob(blob, token)
-//         } catch {
-//             Toast.error('图片下载失败')
-//         }
-//     },
-//     [getLastedToken, tryLoadBlob],
-// )
+    const token = getLastedToken()
+    try {
+        const items = await navigator.clipboard.read()
+        if (items.length > 0) {
+            const item = items.at(0)
+            const blob = await item.getType(item.types[0])
+            await tryLoadBlob(blob, token)
+        } else {
+            message.error('未读取到内容!')
+        }
+    } catch {
+        message.error('读取剪切板失败!')
+    }
+}
+
+const onImportOk = async ({ url }) => {
+    appState.value = AppState.Loading
+    const token = getLastedToken()
+    try {
+        const response = await fetch(url)
+        const blob = await response.blob()
+        await tryLoadBlob(blob, token)
+    } catch {
+        message.error('图片下载失败!')
+    }
+}
+
 // const { importModal, openImportModal } = useImportModal(onImportOk)
 // const onImportFromUrl = useCallback(() => {
 //     setImportDropdownVisible(false)
@@ -150,7 +145,8 @@ import { createPixelator } from '../../../../utils/pixelator'
     <div class="generator-background">
         <div class="generator generator-background">
             <div class="generator-container">
-                <n-upload multiple directory-dnd action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5">
+                <n-upload multiple directory-dnd action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5"
+                    class="upload">
                     <n-upload-dragger>
                         <div style="margin-bottom: 12px">
                             <n-icon size="48" :depth="3" :component="CloudUploadOutlined" />
@@ -163,7 +159,7 @@ import { createPixelator } from '../../../../utils/pixelator'
                         </n-p>
                     </n-upload-dragger>
                 </n-upload>
-                <n-slider v-model:value="value" :step="10" />
+                <n-slider v-model:value="value" :step="10" class="slider" />
             </div>
         </div>
         <div class="btn">
@@ -177,22 +173,41 @@ import { createPixelator } from '../../../../utils/pixelator'
     width: 90%;
     height: 100%;
     margin: 0 auto;
-    position: relative;
 
     border: 1px solid #808080;
     border-radius: 6px;
     background-color: #f3f3f3;
+    transform: rotate(-6deg);
+    transform-origin: right bottom;
 
     .generator {
         display: flex;
+        justify-content: space-between;
         width: 100%;
-        position: absolute;
-        left: 30px;
-        bottom: 30px;
+        height: 100%;
+
+        transform: rotate(6deg);
 
         .generator-container {
             display: flex;
             padding: 0;
+
+            .upload {
+                flex: 3;
+
+                .n-upload-dragger {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    height: 428px;
+
+                    border: none;
+                }
+            }
+
+            .slider {
+                flex: 1;
+            }
         }
     }
 
@@ -203,8 +218,8 @@ import { createPixelator } from '../../../../utils/pixelator'
         width: 60px;
         height: 60px;
         position: absolute;
-        right: -32px;
-        bottom: 0;
+        right: -10px;
+        bottom: -10px;
 
         font-size: 50px;
         border-radius: 50%;
