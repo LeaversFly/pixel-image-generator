7cd5b51685782fff15099a91a41b68756ae6999c
Gin_99 <3348483879@qq.com>
2023-06-17 23:35:17 +0800

    fix: 解决报错

--- src/utils/pixelator/index.js
+++ src/utils/pixelator/index.js
@@ -2,6 +2,6 @@ import Pixelator from './Pixelator'
 import PixelatorMain from './main'
 import PixelatorWorker from './pixelator-worker'
 
-export const createPixelator = onUpdate => Pixelator = window.Worker
-    ? onUpdate => new PixelatorWorker(onUpdate)
-    : onUpdate => new PixelatorMain(onUpdate)
+export const createPixelator = onUpdate => {
+    return new PixelatorWorker(onUpdate)
+}
