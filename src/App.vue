bbb617d8a68daee96c888805577dd5107824c015
Gin_99 <3348483879@qq.com>
2023-07-13 17:08:35 +0800

    feat: 配置主题

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { NMessageProvider } from 'naive-ui'
+import themeOverrides from './config/theme'
 </script>
 
 <template>
-  <n-message-provider>
-    <router-view></router-view>
-  </n-message-provider>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-message-provider>
+      <router-view></router-view>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <style></style>
---
+++ src/config/theme.js
@@ -0,0 +1,13 @@
+const themeOverrides = {
+    common: {
+        primaryColor: '#e66061',
+        primaryColorHover: '#CF523A',
+        primaryColorPressed: '#e66061',
+        primaryColorSuppl: '#4098FC'
+    },
+    Button: {
+        textColor: '#FF0000'
+    }
+}
+
+export default themeOverrides
